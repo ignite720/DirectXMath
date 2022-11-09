1f99a02001de5c4b1d961fc0b077537077fcbcc1
Chuck Walbourn <chuckw@microsoft.com>
2022-11-09 13:53:54 -0800

    Improve support for /Zc:arm64-aliased-neon-types-

--- Inc/DirectXMath.h
+++ Inc/DirectXMath.h
@@ -429,7 +429,7 @@ namespace DirectX
 #elif defined(_XM_SSE_INTRINSICS_)
         inline operator __m128i() const noexcept { return _mm_castps_si128(v); }
         inline operator __m128d() const noexcept { return _mm_castps_pd(v); }
-#elif defined(_XM_ARM_NEON_INTRINSICS_) && defined(__GNUC__)
+#elif defined(_XM_ARM_NEON_INTRINSICS_) && (defined(__GNUC__) || defined(_ARM64_DISTINCT_NEON_TYPES))
         inline operator int32x4_t() const noexcept { return vreinterpretq_s32_f32(v); }
         inline operator uint32x4_t() const noexcept { return vreinterpretq_u32_f32(v); }
 #endif
@@ -448,7 +448,7 @@ namespace DirectX
 #elif defined(_XM_SSE_INTRINSICS_)
         inline operator __m128i() const noexcept { return _mm_castps_si128(v); }
         inline operator __m128d() const noexcept { return _mm_castps_pd(v); }
-#elif defined(_XM_ARM_NEON_INTRINSICS_) && defined(__GNUC__)
+#elif defined(_XM_ARM_NEON_INTRINSICS_) && (defined(__GNUC__) || defined(_ARM64_DISTINCT_NEON_TYPES))
         inline operator int32x4_t() const noexcept { return vreinterpretq_s32_f32(v); }
         inline operator uint32x4_t() const noexcept { return vreinterpretq_u32_f32(v); }
 #endif
@@ -467,7 +467,7 @@ namespace DirectX
 #elif defined(_XM_SSE_INTRINSICS_)
         inline operator __m128i() const noexcept { return _mm_castps_si128(v); }
         inline operator __m128d() const noexcept { return _mm_castps_pd(v); }
-#elif defined(_XM_ARM_NEON_INTRINSICS_) && defined(__GNUC__)
+#elif defined(_XM_ARM_NEON_INTRINSICS_) && (defined(__GNUC__) || defined(_ARM64_DISTINCT_NEON_TYPES))
         inline operator int32x4_t() const noexcept { return vreinterpretq_s32_f32(v); }
         inline operator uint32x4_t() const noexcept { return vreinterpretq_u32_f32(v); }
 #endif
@@ -486,7 +486,7 @@ namespace DirectX
 #elif defined(_XM_SSE_INTRINSICS_)
         inline operator __m128i() const noexcept { return _mm_castps_si128(v); }
         inline operator __m128d() const noexcept { return _mm_castps_pd(v); }
-#elif defined(_XM_ARM_NEON_INTRINSICS_) && defined(__GNUC__)
+#elif defined(_XM_ARM_NEON_INTRINSICS_) && (defined(__GNUC__) || defined(_ARM64_DISTINCT_NEON_TYPES))
         inline operator int32x4_t() const noexcept { return vreinterpretq_s32_f32(v); }
         inline operator uint32x4_t() const noexcept { return vreinterpretq_u32_f32(v); }
 #endif
